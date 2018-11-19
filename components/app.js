4e79cf45d1ac08393890a953013ee67254fb42d1
Your Name <you@example.com>
2018-11-20 02:23:47 +0300

    'as'

--- components/app.js
+++ components/app.js
@@ -6,24 +6,16 @@ const APP_LANGUAGE = 'ru';
 document.addEventListener('click', (e) => {
   const targetEl = e.target;
   const sources = targetEl.dataset.category;
-
   if (targetEl.classList.contains('tab--active')) {
     return;
   }
-
   if (targetEl.classList.contains('tab')) {
     const tabs = Array.from(document.querySelectorAll('.tab'));
     tabs.forEach((tab) => {
       tab.className = 'tab';
     });
-
     document.querySelector('.preloader').style.display = 'block';
-    console.log(targetEl.classList.contains('tab'));
     targetEl.classList.add('tab--active');
-    console.log(targetEl.classList);
-
-
-
     getArticles(`https://newsapi.org/v2/top-headlines?sources=${sources}&apiKey=${API_KEY}`);
   }
 });
